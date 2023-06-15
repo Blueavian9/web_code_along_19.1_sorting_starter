d7d5fa4cfe73079139cca53408761cd722aff404
Cesar Aguilar <Blueavian9@gmail.com>
2023-06-14 22:06:43 -0700

    Initial commit

--- temp.js
+++ temp.js
@@ -4,35 +4,42 @@ const merge = (leftArr, rightArr) => {
   let leftIndex = 0;
   let rightIndex = 0;
 
-  while (leftIndex < leftArray.length || rightIndex < rightArray.length) {
-    const leftEl = leftArr[leftIndex];  
+  while (leftIndex <= leftArr.length && rightIndex <=
+     rightArr.length) {
+    const leftEl = leftArr[leftIndex];
     const rightEl = rightArr[rightIndex];
 
-    if (leftEl < rightEl) {
-        output.push(leftEl);
-        leftIndex++;
+    if (leftEl <= rightEl) {
+      output.push(leftEl);
+      leftIndex++;
     } else {
-        output.push(rightEl); 
-          rightIndex++;
+      output.push(rightEl);
+      rightIndex++;
     }
   }
-}
-
-
-// recursion
-const mergeSort = (array) => {
-  return array;
+  return [
+    ...output,
+    ...leftArr.slice(leftIndex),
+    ...rightArr.slice(rightIndex),
+  ];
 };
 
-const middleIndex = Math.floor(array.length / 2);
-const leftArr = array.slice(0, middleIndex);
-const rightArr = array.slice(middleIndex);
+console.log(merge([3, 6], [2, 4]));
 
-return merge(mergeSort(leftArr), mergeSort(rightArr));
+// recursion
+// const mergeSort = (array) => {
+//   return array;
+// };
 
-mergeSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+// const middleIndex = Math.floor(array.length / 2);
+// const leftArr = array.slice(0, middleIndex);
+// const rightArr = array.slice(middleIndex);
 
-//[1, 4, 2, 8, 345]
+// return merge(mergeSort(leftArr), mergeSort(rightArr));
+
+// mergeSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+
+// //[1, 4, 2, 8, 345]
 
 //[1, 4, 2] [8, 345]
 
